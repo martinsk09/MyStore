24a7e2edb81de2f1610c7d675d0a7a6954ab3da3
Martins Kayode <martinsk09@gmail.com>
2023-02-23 07:05:55 +0100

    StartedDev

---
+++ src/app/components/confirmation/confirmation.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConfirmationComponent } from './confirmation.component';
+
+
+
+@NgModule({
+  declarations: [
+    ConfirmationComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ConfirmationModule { }
---
+++ src/app/components/product-item-detail/product-item-detail.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductItemDetailComponent } from './product-item-detail.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductItemDetailComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProductItemDetailModule { }
